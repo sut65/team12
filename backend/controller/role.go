81d1926f86858d9ca0d07c755adc7367b9fabb48
aamjazrk <aamaloha2244@gmail.com>
2023-01-23 17:16:30 +0700

    role controller - close #44

--- backend/controller/role.go
+++ backend/controller/role.go
@@ -7,21 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// POST /roles
-func CreateRole(c *gin.Context) {
-	var role entity.Role
-	if err := c.ShouldBindJSON(&role); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	if err := entity.DB().Create(&role).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": role})
-}
-
 // GET /role/:id
 func GetRole(c *gin.Context) {
 	var role entity.Role
@@ -45,34 +30,6 @@ func ListRole(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": roles})
 }
 
-// DELETE /roles/:id
-func DeleteRole(c *gin.Context) {
-	id := c.Param("id")
-	if tx := entity.DB().Exec("DELETE FROM roles WHERE id = ?", id); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "role not found"})
-		return
-	}
 
-	c.JSON(http.StatusOK, gin.H{"data": id})
-}
 
-// PATCH /roles
-func UpdateRole(c *gin.Context) {
-	var role entity.Role
-	if err := c.ShouldBindJSON(&role); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
 
-	if tx := entity.DB().Where("id = ?", role.ID).First(&role); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "shelf not found"})
-		return
-	}
-
-	if err := entity.DB().Save(&role).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"data": role})
-}
