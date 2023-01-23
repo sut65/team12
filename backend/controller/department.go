83c5b1341e6def0aeaff209445d3813c5ac75516
aamjazrk <aamaloha2244@gmail.com>
2023-01-23 17:25:15 +0700

    department controller - close #19

---
+++ backend/controller/department.go
@@ -0,0 +1,56 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/aamjazrk/team12/entity"
+	"github.com/gin-gonic/gin"
+)
+
+// POST /departments
+func CreateDepartment(c *gin.Context) {
+	var role entity.Role
+	if err := c.ShouldBindJSON(&role); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := entity.DB().Create(&role).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": role})
+}
+
+// GET /department/:id
+func GetDepartment(c *gin.Context) {
+	var department entity.Department
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM departments WHERE id = ?", id).Scan(&department).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": department})
+}
+
+// GET /departments
+func ListDepartment(c *gin.Context) {
+	var departments []entity.Department
+	if err := entity.DB().Raw("SELECT * FROM departments").Scan(&departments).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": departments})
+}
+func ListDepartmentByRole(c *gin.Context) {
+	var departments []entity.Department
+	id := c.Param("role_id")
+	if err := entity.DB().Raw("SELECT * FROM departments where role_id = ?", id).Scan(&departments).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": departments})
+}
