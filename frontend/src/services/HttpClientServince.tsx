2634a2d49c166dc76eace243c520ef68f87cfbed
Ksis123 <khemkhemsiwa555@gmail.com>
2023-01-24 23:53:46 +0700

    create UI ManageBed - close #72

--- frontend/src/services/HttpClientServince.tsx
+++ frontend/src/services/HttpClientServince.tsx
@@ -1,31 +1,132 @@
+
+import { ManageBedInterface } from "../interfaces/imanagebed/IManageBed";
+
 import { SigninInterface } from "../interfaces/ISignin";
+
 const apiUrl = "http://localhost:8080";
 
 async function Login(data: SigninInterface) {
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    };
-  
-    let res = await fetch(`${apiUrl}/login`, requestOptions)
-      .then((response) => response.json())
-      .then((res) => {
-        if (res.data) {
-          console.log(res.data)
-          localStorage.setItem("token", res.data.token);
-          localStorage.setItem("id", res.data.id);
-          localStorage.setItem("email", res.data.email);
-          localStorage.setItem("role", res.data.role);
-          return res.data;
-        } else {
-          return false;
-        }
-      });
-  
-    return res;
-  }
-  
-  export {
-    Login
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
   };
+
+  let res = await fetch(`${apiUrl}/login`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        console.log(res.data)
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("id", res.data.id);
+        localStorage.setItem("email", res.data.email);
+        localStorage.setItem("role", res.data.role);
+        return res.data;
+      } else {
+        return false;
+      }
+    });
+
+  return res;
+}
+
+// -------------------------------------ManageBed-----------------------------------------------------------------
+
+async function GetManageBed() {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  let res = await fetch(`${apiUrl}/managebeds`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data;
+      } else {
+        return false;
+      }
+    });
+
+  return res;
+}
+
+async function GetBed() {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  let res = await fetch(`${apiUrl}/beds`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data;
+      } else {
+        return false;
+      }
+    });
+
+  return res;
+}
+
+async function GetBedStatus() {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  let res = await fetch(`${apiUrl}/bedstatuses`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data;
+      } else {
+        return false;
+      }
+    });
+
+  return res;
+}
+
+async function CreateManageBed(data: ManageBedInterface) {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  let res = await fetch(`${apiUrl}/managebeds`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data;
+      } else {
+        return false;
+      }
+    });
+
+  return res;
+}
+
+export {
+    Login,
+    
+    
+    GetManageBed,
+    GetBed,
+    GetBedStatus,
+    CreateManageBed,
+};
