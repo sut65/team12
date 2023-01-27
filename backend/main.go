9cbce67a2f6e388ed8b3b52958ebc766d7698fd2
B6238285 <b6238285@g.sut.ac.th>
2023-01-27 19:59:29 +0700

    edit service patient - close #211

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/aamjazrk/team12/controller"
 	"github.com/aamjazrk/team12/entity"
-
 	"github.com/aamjazrk/team12/middlewares"
 
 	//"github.com/aamjazrk/team12/middlewares"
@@ -21,128 +20,127 @@ func main() {
 	{
 		router.Use(middlewares.Authorizes())
 		{
-	//router.GET("/bills", controller.ListPatient)
-	//router.GET("/bill/:id", controller.GetPatient)
-	//router.POST("/createbills", controller.UpdatePatient)
+			//router.GET("/bills", controller.ListPatient)
+			//router.GET("/bill/:id", controller.GetPatient)
+			//router.POST("/createbills", controller.UpdatePatient)
 
-	// Get by id
-	router.GET("/employee/get/:id", controller.GetEmployee)
-	// List
-	router.GET("/employees/list", controller.ListEmployee)
-	// Create
-	router.POST("/employee/create", controller.CreateEmployee)
-	// UPDATE
-	router.PATCH("/employee/update", controller.UpdateEmployee)
-	// DELETE
-	router.DELETE("/employees/delete/:id", controller.DeleteEmployee)
-	// ----------------- Employee ----------------------------
-	// Role Routes
-	router.GET("/roles/list", controller.ListRole)
-	router.GET("/role/get/:id", controller.GetRole)
-	// router.POST("/roles", controller.CreateRole)
-	// router.PATCH("/roles", controller.UpdateRole)
-	// router.DELETE("/roles/:id", controller.DeleteRole)
-	// Gender
-	router.GET("/genders/list", controller.ListGender)
-	router.GET("/gender/get/:id", controller.GetGender)
-	// Department
-	router.GET("/departments/list", controller.ListDepartment)
-	router.GET("/department/get/:id", controller.GetDepartment)
-	router.GET("/departmentbyrole/get/:id", controller.ListDepartmentByRole)
+			// Get by id
+			router.GET("/employee/get/:id", controller.GetEmployee)
+			// List
+			router.GET("/employees/list", controller.ListEmployee)
+			// Create
+			router.POST("/employee/create", controller.CreateEmployee)
+			// UPDATE
+			router.PATCH("/employee/update", controller.UpdateEmployee)
+			// DELETE
+			router.DELETE("/employees/delete/:id", controller.DeleteEmployee)
+			// ----------------- Employee ----------------------------
+			// Role Routes
+			router.GET("/roles/list", controller.ListRole)
+			router.GET("/role/get/:id", controller.GetRole)
+			// router.POST("/roles", controller.CreateRole)
+			// router.PATCH("/roles", controller.UpdateRole)
+			// router.DELETE("/roles/:id", controller.DeleteRole)
+			// Gender
+			router.GET("/genders/list", controller.ListGender)
+			router.GET("/gender/get/:id", controller.GetGender)
+			// Department
+			router.GET("/departments/list", controller.ListDepartment)
+			router.GET("/department/get/:id", controller.GetDepartment)
+			router.GET("/departmentbyrole/get/:id", controller.ListDepartmentByRole)
 
-	// ------------------ Lab X ray ------------------------------
-	// labtype Routes
-	router.GET("/labtypes/list", controller.ListLabType)
-	router.GET("/labtype/get/:id", controller.GetLabType)
-	// labxray Routes
-	// Get by id
-	router.GET("/labxray/get/:id", controller.GetLabXray)
-	// List
-	router.GET("/labxrays/list", controller.ListLabXray)
-	// Create
-	router.POST("/labxray/create", controller.CreateLabXray)
-	// UPDATE
-	router.PATCH("/labxray/update", controller.UpdateLabXray)
-	// DELETE
-	router.DELETE("/labxrays/delete/:id", controller.DeleteLabXray)
+			// ------------------ Lab X ray ------------------------------
+			// labtype Routes
+			router.GET("/labtypes/list", controller.ListLabType)
+			router.GET("/labtype/get/:id", controller.GetLabType)
+			// labxray Routes
+			// Get by id
+			router.GET("/labxray/get/:id", controller.GetLabXray)
+			// List
+			router.GET("/labxrays/list", controller.ListLabXray)
+			// Create
+			router.POST("/labxray/create", controller.CreateLabXray)
+			// UPDATE
+			router.PATCH("/labxray/update", controller.UpdateLabXray)
+			// DELETE
+			router.DELETE("/labxrays/delete/:id", controller.DeleteLabXray)
 
-	// ----------------- Patient -----------------------------
+			// ----------------- Patient -----------------------------
 
-	// Patient Routes
-	router.GET("/patients/list", controller.ListPatient)
+			// Patient Routes
+			router.GET("/patients/list", controller.ListPatient)
 
-	// ----------------- ManageBed ----------------------------
-	// Bed Routes
-	router.GET("/beds", controller.ListBed)
-	router.GET("/bed/:id", controller.GetBed)
+			// ----------------- ManageBed ----------------------------
+			// Bed Routes
+			router.GET("/beds", controller.ListBed)
+			router.GET("/bed/:id", controller.GetBed)
 
-	// BedStatus Routes
-	router.GET("/bedstatuses", controller.ListBedStatus)
-	router.GET("/bedstatus/:id", controller.GetBedStatus)
+			// BedStatus Routes
+			router.GET("/bedstatuses", controller.ListBedStatus)
+			router.GET("/bedstatus/:id", controller.GetBedStatus)
 
-	// ManageBed Routes
-	router.GET("/managebeds", controller.ManageBed)
-	router.GET("/managebed/:id", controller.GetManageBed)
-	router.POST("/managebeds", controller.CreateManageBed)
-	router.PATCH("/managebeds", controller.UpdateManageBed)
-	router.DELETE("/managebeds/:id", controller.DeleteManageBed)
+			// ManageBed Routes
+			router.GET("/managebeds", controller.ManageBed)
+			router.GET("/managebed/:id", controller.GetManageBed)
+			router.POST("/managebeds", controller.CreateManageBed)
+			router.PATCH("/managebeds", controller.UpdateManageBed)
+			router.DELETE("/managebeds/:id", controller.DeleteManageBed)
 
-	// ----------------- MedicalSlip ----------------------------
-	// MedicalSlip Routes
-	router.GET("/medicalslips", controller.ListMedicalSlip)
-	router.GET("/medicalslip/:id", controller.GetMedicalSlip)
-	router.POST("/medicalslips", controller.CreateMedicalSlip)
-	router.PATCH("/medicalslips", controller.UpdateMedicalSlip)
-	router.DELETE("/medicalslips/:id", controller.DeleteMedicalSlip)
+			// ----------------- MedicalSlip ----------------------------
+			// MedicalSlip Routes
+			router.GET("/medicalslips", controller.ListMedicalSlip)
+			router.GET("/medicalslip/:id", controller.GetMedicalSlip)
+			router.POST("/medicalslips", controller.CreateMedicalSlip)
+			router.PATCH("/medicalslips", controller.UpdateMedicalSlip)
+			router.DELETE("/medicalslips/:id", controller.DeleteMedicalSlip)
 
-	// ------------------ MST ------------------------------
-	// hospital Routes
-	router.GET("/hospitals/list", controller.ListHospital)
-	router.GET("/hospital/get/:id", controller.GetHospital)
-	// MST Routes
-	router.GET("/mst/get/:id", controller.GetMST)             // Get by id
-	router.GET("/msts/list", controller.ListMST)  			 // List
-	router.POST("/mst/create", controller.CreateMST) 		 // Create
-	router.PATCH("/mst/update", controller.UpdateMST)		 // UPDATE
-	router.DELETE("/msts/delete/:id", controller.DeleteMST) 	 // DELETE
+			// ------------------ MST ------------------------------
+			// hospital Routes
+			router.GET("/hospitals/list", controller.ListHospital)
+			router.GET("/hospital/get/:id", controller.GetHospital)
+			// MST Routes
+			router.GET("/mst/get/:id", controller.GetMST)           // Get by id
+			router.GET("/msts/list", controller.ListMST)            // List
+			router.POST("/mst/create", controller.CreateMST)        // Create
+			router.PATCH("/mst/update", controller.UpdateMST)       // UPDATE
+			router.DELETE("/msts/delete/:id", controller.DeleteMST) // DELETE
 
-	
-	// ----------------- Kool ผ่าตัด ----------------------------
-	// Routes OperatingRoom
-	router.GET("/ListOperatingRoom", controller.ListOperatingRoom)
-	router.GET("/GetOperatingRoom/:id", controller.GetOperatingRoom)
-	// Routes Specialist
-	router.GET("/ListSpecialist", controller.ListSpecialist)
-	router.GET("/GetSpecialist/:id", controller.GetSpecialist)
-	// Routes SurgeryState
-	router.GET("/ListSurgeryState", controller.ListSurgeryState)
-	router.GET("/GetSurgeryState/:id", controller.GetSurgeryState)
-	// Routes SurgeryType
-	router.GET("/ListSurgeryType", controller.ListSurgeryType)
-	router.GET("/GetSurgeryType/:id", controller.GetSurgeryType)
-	// Routes ORrecord
-	router.GET("/GetORrecord/get/:id", controller.GetORrecord)
-	router.GET("/ListORrecord/list", controller.ListORrecord)
-	router.POST("/CreateORrecord/create", controller.CreateORrecord)
-	router.PATCH("/UpdateORrecord/update", controller.UpdateORrecord)
-	router.DELETE("/DeleteORrecord/delete/:id", controller.DeleteORrecord)
+			// ----------------- Kool ผ่าตัด ----------------------------
+			// Routes OperatingRoom
+			router.GET("/ListOperatingRoom", controller.ListOperatingRoom)
+			router.GET("/GetOperatingRoom/:id", controller.GetOperatingRoom)
+			// Routes Specialist
+			router.GET("/ListSpecialist", controller.ListSpecialist)
+			router.GET("/GetSpecialist/:id", controller.GetSpecialist)
+			// Routes SurgeryState
+			router.GET("/ListSurgeryState", controller.ListSurgeryState)
+			router.GET("/GetSurgeryState/:id", controller.GetSurgeryState)
+			// Routes SurgeryType
+			router.GET("/ListSurgeryType", controller.ListSurgeryType)
+			router.GET("/GetSurgeryType/:id", controller.GetSurgeryType)
+			// Routes ORrecord
+			router.GET("/GetORrecord/get/:id", controller.GetORrecord)
+			router.GET("/ListORrecord/list", controller.ListORrecord)
+			router.POST("/CreateORrecord/create", controller.CreateORrecord)
+			router.PATCH("/UpdateORrecord/update", controller.UpdateORrecord)
+			router.DELETE("/DeleteORrecord/delete/:id", controller.DeleteORrecord)
 
-	// ----------------- Kool แจ้งบำรุง ----------------------------
-	// Routes ClassProb
-	router.GET("/ListClassProb", controller.ListClassProb)
-	router.GET("/GetClassProb/:id", controller.GetClassProb)
-	// Routes NumPlace
-	router.GET("/ListNumPlace", controller.ListNumPlace)
-	router.GET("/GetNumPlace/:id", controller.GetNumPlace)
-	// Routes Problem
-	router.GET("/ListProblem", controller.ListProblem)
-	router.GET("/GetProblem/:id", controller.GetProblem)
-	// Routes ProblemReport
-	router.GET("/ListProblemReport", controller.ListProblemReport)
-	router.GET("/GetProblemReport/:id", controller.GetProblemReport)
-	router.POST("/CreateProblemReport/create", controller.CreateProblemReport)
-	router.PATCH("/UpdateProblemReport/update", controller.UpdateProblemReport)
-	router.DELETE("/DeleteProblemReport/delete/:id", controller.DeleteProblemReport)
+			// ----------------- Kool แจ้งบำรุง ----------------------------
+			// Routes ClassProb
+			router.GET("/ListClassProb", controller.ListClassProb)
+			router.GET("/GetClassProb/:id", controller.GetClassProb)
+			// Routes NumPlace
+			router.GET("/ListNumPlace", controller.ListNumPlace)
+			router.GET("/GetNumPlace/:id", controller.GetNumPlace)
+			// Routes Problem
+			router.GET("/ListProblem", controller.ListProblem)
+			router.GET("/GetProblem/:id", controller.GetProblem)
+			// Routes ProblemReport
+			router.GET("/ListProblemReport", controller.ListProblemReport)
+			router.GET("/GetProblemReport/:id", controller.GetProblemReport)
+			router.POST("/CreateProblemReport/create", controller.CreateProblemReport)
+			router.PATCH("/UpdateProblemReport/update", controller.UpdateProblemReport)
+			router.DELETE("/DeleteProblemReport/delete/:id", controller.DeleteProblemReport)
 
 		}
 	}
