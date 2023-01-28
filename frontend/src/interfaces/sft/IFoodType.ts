83bf3c974f9c63000e049ddefbd707c9c5f475a6
siwanastudio <111453181+siwanastudio@users.noreply.github.com>
2023-01-28 14:37:23 +0700

    update interface sft - close #108

---
+++ frontend/src/interfaces/sft/IFoodType.ts
@@ -0,0 +1,4 @@
+export interface FoodTypeInterface{
+    ID?: number,
+    FoodType?: string ;
+}
---
+++ frontend/src/interfaces/sft/ISFT.ts
@@ -0,0 +1,16 @@
+import { PrincipalDiagnosisInterface } from "../principaldiagnosis/IPrincipalDiagnosis";
+import { FoodTypeInterface } from "./IFoodType";
+import { PatientInterface } from "../patient/IPatient";
+import { EmployeeInterface } from "../employee/IEmployee";
+export interface SFTInterface{
+    ID:          number,
+    PatientID?:   number ;
+    Patient?:     PatientInterface;
+    PrincipalDiagnosisID?:     number,
+    PrincipalDiagnosis?:      PrincipalDiagnosisInterface,
+    FoodTypeID?:  number,
+    FoodType?:    FoodTypeInterface,
+    DoctorID?:    number,
+    Doctor?:      EmployeeInterface,
+    Date?:    Date | null;
+}
