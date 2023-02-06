f3170a227a67433d3915d7743558f7c57b99311b
aamjazrk <aamaloha2244@gmail.com>
2023-02-06 22:12:02 +0700

    validate Lastname - close #64

--- backend/UnitTest/Employee_validation_test.go
+++ backend/UnitTest/Employee_validation_test.go
@@ -49,5 +49,43 @@ func TestEmployeeValidate(t *testing.T) {
 
 		g.Expect(err.Error()).To(gomega.Equal("FirstName cannot be blank"))
 	})
+	t.Run("LastName must have only character", func(t *testing.T) {
+		e := Employee{
+			FirstName: "Sirinya",
+			LastName:  "11",
+			Civ:       "1236479869777",
+			Phone:     "0624445678",
+			Email:     "si@gmail.com",
+			Password:  "zaq1wsXp",
+			Address:   "fgfhh",
+		}
+
+		ok, err := govalidator.ValidateStruct(e)
+
+		g.Expect(ok).NotTo(gomega.BeTrue())
+
+		g.Expect(err).ToNot(gomega.BeNil())
+
+		g.Expect(err.Error()).To(gomega.Equal("LastName must have only character"))
+	})
+	t.Run("LastName cannot be blank", func(t *testing.T) {
+		e := Employee{
+			FirstName: "Sirinya",
+			LastName:  "",
+			Civ:       "1236479869777",
+			Phone:     "0624445678",
+			Email:     "si@gmail.com",
+			Password:  "zaq1wsXp",
+			Address:   "fgfhh",
+		}
+
+		ok, err := govalidator.ValidateStruct(e)
+
+		g.Expect(ok).NotTo(gomega.BeTrue())
+
+		g.Expect(err).ToNot(gomega.BeNil())
+
+		g.Expect(err.Error()).To(gomega.Equal("LastName cannot be blank"))
+	})
 	
 }
